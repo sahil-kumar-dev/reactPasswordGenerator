1565d4672c08ec861ee286db2681c4a67dc91a48
Sahil kumar <sahilkumarofficial05@gmail.com>
2023-09-20 22:18:57 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,84 @@
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
+import { FaClipboard } from "react-icons/fa6";
+import './style.css'
 
 function App() {
 
   const [length, setlength] = useState(8)
   const [numbers, setnumbers] = useState(false)
   const [charcter, setcharcter] = useState(false)
+  const [uppercase, setuppercase] = useState(false)
   const [password, setpassword] = useState("")
 
-  const passwordGenerator = useCallback (()=>{
-    let pass=""
-    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  const passwordRef = useRef(null)
 
-    if(charcter) str+="!@#$%^&*"
-    if(numbers)str+="0123456789"
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "abcdefghijklmnopqrstuvwxyz"
 
-    for(let i=1; i<=length; i++){
-      let char=Math.floor(Math.random()*str.length+1);
-      pass+=str.charAt(char);
+    if (charcter) str += "!@#$%^&*"
+    if (numbers) str += "0123456789"
+    if (uppercase) str += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
     }
     setpassword(pass);
-  },[numbers,charcter,length]);
+  }, [numbers, charcter, length, uppercase]);
 
-  useEffect(()=>{
+  const copyPassword = useCallback(() => {
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(() => {
     passwordGenerator()
-  },[length],[charcter],[numbers])
+  }, [length, numbers, charcter, uppercase])
   return (
-    <>
-      <h1 className="text-2xl text-purple-500 ">Password Generator</h1>
-      <input 
-        type="text" 
-        value={password}
-        readOnly
-      />
-      <input 
-        type="range" 
-        min={8}
-        max={20}
-        value={length}
-        onChange={(e)=>setlength(e.target.value)}
-      />
-      {length}      
-      <label htmlFor="numbers">Numbers</label>
-      <input 
-        type="checkbox" 
-        id="numbers" 
-        onChange={()=>setnumbers(prev=>!prev)}
-      />
-      <label htmlFor="charcters">chracters</label>
-      <input 
-        type="checkbox" 
-        id="charcters" 
-        onChange={()=>setcharcter(prev=>!prev)}
-      />
-    </>
+    <div className="container">
+      <div className="heading">
+        <h2>Password Generator</h2>
+      </div>
+      <div className="answer">
+        <input type="text" id="result" value={password} readOnly ref={passwordRef} />
+        <button className="btn" onClick={copyPassword}>{<FaClipboard></FaClipboard>}</button>
+      </div>
+      <div className="settings">
+        <div className="options">
+          <label htmlFor="slide">Password Length</label>
+          <input type="range" name="" className="slider" id="slide" min="4" max="30" value={length} onChange={(e) => setlength(e.target.value)} />
+          <div>{length < 10 ? `0${length}` : length}</div>
+        </div>
+        <div className="options">
+          <div>Include Uppercase</div>
+          <div className="toggel-container">
+            <input type="checkbox" id="uppercase" onClick={() => setuppercase(prev => !prev)} />
+            <label htmlFor="uppercase" id="toggel">
+              <div className="circle"></div>
+            </label>
+          </div>
+        </div>
+        <div className="options">
+          <div>Include Symbols</div>
+          <div className="toggel-container">
+            <input type="checkbox" id="symbols" onClick={() => setcharcter(prev => !prev)} />
+            <label htmlFor="symbols" id="toggel">
+              <div className="circle"></div>
+            </label>
+          </div>
+        </div>
+        <div className="options">
+          <div>Include Numbers</div>
+          <div className="toggel-container">
+            <input type="checkbox" id="numbers" onClick={() => setnumbers(prev => !prev)} />
+            <label htmlFor="numbers" id="toggel">
+              <div className="circle"></div>
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
